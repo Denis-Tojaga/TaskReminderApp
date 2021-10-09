75a2bdc58cf542e27633545109d7db289602edd8
Denis <denis.tojaga@edu.fit.ba>
2021-10-09 17:07:54 +0200

    //migrating to TaskReminder App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
 
-//angular default modules
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
-
-
-//angular installed modules
-import { GaugeModule } from 'angular-gauge';
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatIconModule } from "@angular/material/icon";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSelectModule } from "@angular/material/select";
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 
 
 @NgModule({
 
   declarations: [
-    AppComponent,
-    SearchBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    GaugeModule.forRoot(),
-    MatFormFieldModule,
-    MatSelectModule,
-    MatIconModule,
-    MatTabsModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
