78ec53e0d5d9ad6d460bcd059fdb086d3ff4afb4
Denis <denis.tojaga@edu.fit.ba>
2021-10-09 17:58:46 +0200

    //adding fake data for tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,32 @@
+export const TASKS = [
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "May 5th at 2:30pm",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Meeting at school",
+        day: "May 6th at 1:30pm",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Food shopping",
+        day: "May 7th at 12:30pm",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Finish the assignment",
+        day: "May 10th at 5:30pm",
+        reminder: true
+    },
+    {
+        id: 5,
+        text: "Buy a new phone",
+        day: "May 13th at 9:30am",
+        reminder: false
+    }
+];
