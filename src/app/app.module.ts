fee0dbf5d6347490ff8da067df4a15ff74393865
Denis <denis.tojaga@edu.fit.ba>
2021-10-15 17:41:07 +0200

    //routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
@@ -41,7 +42,8 @@ const appRoutes: Routes = [
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UiService } from 'src/app/services/ui.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
   title: string = "Task Tracker";
   showAddTask: boolean;
   subscription: Subscription;
 
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((returnedValue) => {
       this.showAddTask = returnedValue;
     });
@@ -24,10 +27,12 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
-
   toggleAddTask() {
     this.uiService.toggleAddTask();
   }
 
+  //method that will check if the router.url is the same as the parameter one
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 }
